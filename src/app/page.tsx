acca278a7ee6cae6fb7326027de339eb75a3c79c
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-07-10 20:09:04 +0530

    Added Dark and Light Mode
    Fixed Icon Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import {
   FilledTonalIconButton,
   IconButton,
   OutlinedButton,
+  OutlinedIconButton,
   TextButton,
 } from "@/components/button/button";
 import { Dialog } from "@/components/dialog/dialog";
@@ -29,66 +30,70 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-flow-row grid-cols-2 gap-2 m-2">
-      <Fab size="medium">
-        <Icon slot="icon">edit</Icon>
-      </Fab>
+    <>
+      <h2 className="text-center text-on-primary bg-primary-container p-8 m-2 rounded-xl">Test</h2>
 
-      <div className="flex flex-row gap-2 h-fit">
-        <IconButton>
-          <Icon>add</Icon>
-        </IconButton>
-        <FilledIconButton>
-          <Icon>add</Icon>
-        </FilledIconButton>
-        <FilledTonalIconButton>
-          <Icon>add</Icon>
-        </FilledTonalIconButton>
-        <OutlinedButton>
-          <Icon>add</Icon>
-        </OutlinedButton>
-      </div>
+      <div className="grid grid-flow-row grid-cols-2 gap-2 m-2">
+        <Fab size="medium">
+          <Icon slot="icon">edit</Icon>
+        </Fab>
 
-      <FilledButton>
-        Click me
-        <svg slot="icon" viewBox="0 0 48 48">
-          <path d="M6 40V8l38 16Zm3-4.65L36.2 24 9 12.5v8.4L21.1 24 9 27Zm0 0V12.5 27Z" />
-        </svg>
-      </FilledButton>
-      <FilledTonalButton>
-        Click me
-        <svg slot="icon" viewBox="0 0 48 48">
-          <path d="M6 40V8l38 16Zm3-4.65L36.2 24 9 12.5v8.4L21.1 24 9 27Zm0 0V12.5 27Z" />
-        </svg>
-      </FilledTonalButton>
-      <TextButton onClick={openDialog}>Open</TextButton>
-      <OutlinedButton href="https://a3group.co.in">
-        Click me
-        <svg slot="icon" viewBox="0 0 48 48">
-          <path d="M6 40V8l38 16Zm3-4.65L36.2 24 9 12.5v8.4L21.1 24 9 27Zm0 0V12.5 27Z" />
-        </svg>
-      </OutlinedButton>
-      <ElevatedButton disabled>
-        Click me
-        <svg slot="icon" viewBox="0 0 48 48">
-          <path d="M6 40V8l38 16Zm3-4.65L36.2 24 9 12.5v8.4L21.1 24 9 27Zm0 0V12.5 27Z" />
-        </svg>
-      </ElevatedButton>
-
-      <Dialog ref={dialogRef}>
-        <div slot="headline">Dialog title</div>
-        <form slot="content" method="dialog">
-          A simple dialog with free-form content.
-        </form>
-        <div slot="actions">
-          <TextButton onClick={closeDialog} value="cancel">
-            Cancel
-          </TextButton>
-          <TextButton onClick={closeDialog} value="ok">
-            Ok
-          </TextButton>
+        <div className="flex flex-row gap-2 h-fit">
+          <IconButton>
+            <Icon>add</Icon>
+          </IconButton>
+          <FilledIconButton>
+            <Icon>add</Icon>
+          </FilledIconButton>
+          <FilledTonalIconButton>
+            <Icon>add</Icon>
+          </FilledTonalIconButton>
+          <OutlinedIconButton>
+            <Icon>add</Icon>
+          </OutlinedIconButton>
         </div>
-      </Dialog>
-    </div>
+
+        <FilledButton>
+          Click me
+          <svg slot="icon" viewBox="0 0 48 48">
+            <path d="M6 40V8l38 16Zm3-4.65L36.2 24 9 12.5v8.4L21.1 24 9 27Zm0 0V12.5 27Z" />
+          </svg>
+        </FilledButton>
+        <FilledTonalButton>
+          Click me
+          <svg slot="icon" viewBox="0 0 48 48">
+            <path d="M6 40V8l38 16Zm3-4.65L36.2 24 9 12.5v8.4L21.1 24 9 27Zm0 0V12.5 27Z" />
+          </svg>
+        </FilledTonalButton>
+        <TextButton onClick={openDialog}>Open</TextButton>
+        <OutlinedButton href="https://a3group.co.in">
+          Click me
+          <svg slot="icon" viewBox="0 0 48 48">
+            <path d="M6 40V8l38 16Zm3-4.65L36.2 24 9 12.5v8.4L21.1 24 9 27Zm0 0V12.5 27Z" />
+          </svg>
+        </OutlinedButton>
+        <ElevatedButton disabled>
+          Click me
+          <svg slot="icon" viewBox="0 0 48 48">
+            <path d="M6 40V8l38 16Zm3-4.65L36.2 24 9 12.5v8.4L21.1 24 9 27Zm0 0V12.5 27Z" />
+          </svg>
+        </ElevatedButton>
+
+        <Dialog ref={dialogRef}>
+          <div slot="headline">Dialog title</div>
+          <form slot="content" method="dialog">
+            A simple dialog with free-form content.
+          </form>
+          <div slot="actions">
+            <TextButton onClick={closeDialog} value="cancel">
+              Cancel
+            </TextButton>
+            <TextButton onClick={closeDialog} value="ok">
+              Ok
+            </TextButton>
+          </div>
+        </Dialog>
+      </div>
+    </>
   );
 }
